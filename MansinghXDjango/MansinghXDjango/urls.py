51156d01acf60d483d97797d42580c0185e67623
Mansingh <mansinghch90@gmail.com>
2026-06-10 01:35:44 +0530

    Added render() to views.py for requests

--- MansinghXDjango/MansinghXDjango/urls.py
+++ MansinghXDjango/MansinghXDjango/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views  #importing views.py from current directory
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.homeMansingh, name="Home | Mansingh"), 
+    path('aboutMansingh/', views.aboutMansingh, name="About| Mansingh"), 
+    path('connectMansingh/', views.connectMansingh, name="Connect | Mansingh")
+    
 ]
