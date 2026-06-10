3eddf26b1d5a72aa75abd970496efb7577cbdc61
Mansingh <mansinghch90@gmail.com>
2026-06-11 00:49:43 +0530

    add 'theme' to INSTALLED_APPS and declare TAILWIND_APP_NAME = 'theme' in settings.py

--- MansinghXDjango/MansinghXDjango/settings.py
+++ MansinghXDjango/MansinghXDjango/settings.py
@@ -39,8 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'PowerRangers', 
-    'tailwind'
+    'tailwind', 
+    'theme', 
 ]
+
+TAILWIND_APP_NAME = 'theme'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
