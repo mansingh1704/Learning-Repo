09a02fbee038b76897899f63bd64f2a2957df1d0
Mansingh <mansinghch90@gmail.com>
2026-06-10 19:10:43 +0530

    Defined a url path for new app

--- MansinghXDjango/PowerRangers/urls.py
+++ MansinghXDjango/PowerRangers/urls.py
@@ -1,16 +1,11 @@
- # copy paste all this code from main project folder's urls.py
+# copy paste all this code from main project folder's urls.py
 
 
-from django.contrib import admin
 from django.urls import path
 from . import views  #importing views.py from current directory
 
-
+#localhost:8000/PowerRangers
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.homeMansingh, name="Home | Mansingh"), 
-    path('aboutMansingh/', views.aboutMansingh, name="About| Mansingh"), 
-    path('connectMansingh/', views.connectMansingh, name="Connect | Mansingh"),
-    path('')
-    
+    path('', views.all_rangers, name="All Power Rangers"), 
+        
 ]
