e80ad10bab9e47d3d05a8361fa92405a0ef2e299
Mansingh <mansinghch90@gmail.com>
2026-06-12 02:01:31 +0530

    Registered the model(RangerTypes) in models.py

--- MansinghXDjango/MansinghXDjango/settings.py
+++ MansinghXDjango/MansinghXDjango/settings.py
@@ -130,4 +130,4 @@ STATIC_URL = 'static/style.css/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = [os.path.join(BASE_DIR, 'media')]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
