40346a83fb0832cdbc13bf022a4ec1ed7326ff0d
Mansingh <mansinghch90@gmail.com>
2026-06-12 01:30:59 +0530

    Added the url path for the media and the media root

--- MansinghXDjango/MansinghXDjango/urls.py
+++ MansinghXDjango/MansinghXDjango/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views  #importing views.py from current directory
 
 
@@ -28,4 +30,4 @@ urlpatterns = [
    #include a file of a app as when we hit the PowerRangers it will ctrl tranfer to that apps url
 
    path("__reload__/",include("django_browser_reload.urls")),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
