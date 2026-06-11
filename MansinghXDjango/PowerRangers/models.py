edc4e9d206fc9baea44008a3dcdbe53fe7af01aa
Mansingh <mansinghch90@gmail.com>
2026-06-12 01:09:43 +0530

    added a model in models.py with installing Pillow

--- MansinghXDjango/PowerRangers/models.py
+++ MansinghXDjango/PowerRangers/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class RangerTypes(models.Model):
+    RANGER_TYPE_CHOICE = [
+        ('RED', 'RED RANGER'),
+        ('YLW', 'YELLOW RANGER'),
+        ('PNK', 'PINK RANGER'),
+        ('BLU', 'BLUE RANGER'),
+        ('GRN', 'GREEN RANGER'),
+        ('GLD', 'GOLDEN RANGER'),
+    ]
+
+    name = models.CharField(max_length=80)
+    image = models.ImageField(upload_to='Rangers/')
+    date_added = models.DateTimeField(default=timezone.now)
+
+    type = models.CharField(max_length=3, choices = RANGER_TYPE_CHOICE)
+
+
+    
