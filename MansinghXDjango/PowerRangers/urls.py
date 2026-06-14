bf7d330710e69effdaf41505cc0affe0afc6fa2b
Mansingh <mansinghch90@gmail.com>
2026-06-15 01:55:56 +0530

    Made forms.py and importing it from django

--- MansinghXDjango/PowerRangers/urls.py
+++ MansinghXDjango/PowerRangers/urls.py
@@ -7,7 +7,7 @@ from .import views  #importing views.py from current directory
 #localhost:8000/PowerRangers
 urlpatterns = [
     path('', views.all_rangers, name="All Power Rangers"), 
-    path('rangers_stores', views.ranger_store_view , name="All Rangers Stores"), 
+    path('rangers_stores', views.ranger_store_view, name="All Rangers Stores"), 
 
         
 ]
