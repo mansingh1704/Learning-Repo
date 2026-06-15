442b620272ac2f488e37736ce91f50f619874d82
Mansingh <mansinghch90@gmail.com>
2026-06-15 20:33:53 +0530

    Made a class for form in forms.py

--- MansinghXDjango/PowerRangers/forms.py
+++ MansinghXDjango/PowerRangers/forms.py
@@ -1,3 +1,6 @@
 from django import forms
 from .models import RangerTypes
 
+class RangerTypesForm(forms.FORM):
+    ranger_type = forms.ModelChoiceField(queryset=RangerTypes.objects.all(), label="Select Power Ranger Types")
+    
