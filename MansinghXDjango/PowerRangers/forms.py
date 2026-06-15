24db07e2adbce9d4fab5660586f83033e8398b11
Mansingh <mansinghch90@gmail.com>
2026-06-16 00:07:33 +0530

    Made form in views.py for ranger availability in stores and mades layouts in ranger_stores.html

--- MansinghXDjango/PowerRangers/forms.py
+++ MansinghXDjango/PowerRangers/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import RangerTypes
 
-class RangerTypesForm(forms.FORM):
+class RangerTypesForm(forms.Form):
     ranger_type = forms.ModelChoiceField(queryset=RangerTypes.objects.all(), label="Select Power Ranger Types")
-    
+
--- MansinghXDjango/PowerRangers/views.py
+++ MansinghXDjango/PowerRangers/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from .models import RangerTypes
+from .models import RangerTypes ,RangerStores
 from django.shortcuts import get_object_or_404
+from .forms import RangerTypesForm
 
 # Create your views here.
 
@@ -8,5 +9,18 @@ def all_rangers(request):
     rangers = RangerTypes.objects.all()
     return render(request, 'ranger/all_rangers.html', { 'rangers': rangers})
 
+
 def ranger_store_view(request):
-    return render(request, 'ranger/ranger_stores.html')
+    stores = None
+
+    # submitting the form
+    if request.method == 'POST':
+        form = RangerTypesForm(request.POST)
+        if form.is_valid():
+            ranger_types = form.cleaned_data['ranger_type']
+            stores = RangerStores.objects.filter(ranger_type_available= ranger_types)
+    else:
+        form = RangerTypesForm()   
+
+
+    return render(request, 'ranger/ranger_stores.html', {'stores': stores, 'form':form})
